3321071ab14d873b72668f9b5f89d797d847fb93
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-20 04:09:42 -0700

    Add files via upload

---
+++ python/4.py
@@ -0,0 +1,9 @@
+a = 10
+b = 14
+c = 12
+if a >= b and a >= c:
+    print("Largest number is:", a)
+elif b >= a and b >= c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
