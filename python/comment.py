6d98b2455cf43314daba7469c98138f22e706ab2
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-11-11 05:14:03 -0800

    Add files via upload

---
+++ python/comment.py
@@ -0,0 +1,8 @@
+name="dipak patil"
+print(name)
+#im start for pathon learn to 11,11,2024
+#single line comment for python
+"""multiple line comment 
+foe triple  colon 
+is multiple comment in pythone
+"""
