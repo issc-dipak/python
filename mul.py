271ac98ad0184174234cf867f41de8210e74d5fd
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-08-04 06:11:23 -1200

    Create mul.py

---
+++ mul.py
@@ -0,0 +1,7 @@
+ Multiply two numbers with user input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 * num2
+
+print("The result is:", result)
