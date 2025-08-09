12edac4c1105fe29bc6ac7751c2b72d467074a45
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-08-09 23:49:30 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,9 @@
+# Define two strings
+string1 = "Hello, "
+string2 = "World!"
+
+# Concatenate the two strings using the + operator
+combined_string = string1 + string2
+
+# Print the result
+print(combined_string)
