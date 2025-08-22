295a962fede238f2a78d280354a529fe34d8aaa9
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-08-22 21:59:05 +0530

    Create Are.py

---
+++ Are.py
@@ -0,0 +1,21 @@
+# Array using list
+arr = [10, 20, 30, 40, 50]
+
+# Access elements
+print("First element:", arr[0])
+print("Last element:", arr[-1])
+
+# Update element
+arr[2] = 35
+print("Updated array:", arr)
+
+# Append element
+arr.append(60)
+print("After append:", arr)
+
+# Remove element
+arr.remove(20)
+print("After removing 20:", arr)
+
+# Length of array
+print("Length:", len(arr))
