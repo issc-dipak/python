70cb3d82d72c803287012a29a762ccf1ebbadb32
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-09-08 05:48:44 -1200

    Create array1.py

---
+++ array1.py
@@ -0,0 +1,16 @@
+# Array (list) banate hain
+arr = [10, 20, 30, 40, 50]
+
+# Print karna
+print("Array:", arr)
+
+# Access element
+print("First element:", arr[0])
+
+# Update element
+arr[2] = 100
+print("Updated Array:", arr)
+
+# Iterate (loop)
+for x in arr:
+    print("Value:", x)
