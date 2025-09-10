4fb8ae5ad20d15dc0fca33c7718ca9bcba0d3cb0
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-09-10 06:56:58 -1200

    Create commsepr.py

---
+++ commsepr.py
@@ -0,0 +1,3 @@
+name,char = input ("enter comma separated name and character :").split(",")
+print(f"length of your name is {len(name)}")
+print(f "charcter count : {name.lower().count(char.lower())}")
