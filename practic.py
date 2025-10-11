d758b241211ade48428121239ee086787491033a
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-10-11 23:40:55 +0530

    Create practic.py

---
+++ practic.py
@@ -0,0 +1,8 @@
+N = int(input("Enter N: "))
+
+for num in range(2, N + 1):
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
